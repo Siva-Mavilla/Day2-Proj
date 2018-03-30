a25ac668fb61b8844d3bc89ac3d90ec66fbd8155
smavilla <smavilla@DIN80011876.corp.capgemini.com>
2018-03-30 10:17:20 +0530

    My First Proj

---
+++ Day2-Proj/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,12 @@
+package org.cap.demo;
+
+public class SalesClass {
+
+	
+	public void  show() {
+		
+		
+		int[] args ={12,19,20};
+		System.out.println("Show Method");
+	}
+}
---
+++ Day2-Proj/src/main/java/org/cap/demo/Welcome.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class Welcome {
+
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the New World");
+	}
+}
